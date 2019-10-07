40b1b4a21551ff1585e00991c97205a826c3f4bd
MengKe <m1engk1e@gmail.com>
2019-10-07 21:10:22 +0800

    bug fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "ezfile",
-    version = "0.0.1",
+    version = "0.0.3",
     keywords = ["pip", "datacanvas", "eds", "xiaoh"],
     description = "a easy file operation lib",
     long_description = "a easy file operation lib",
@@ -15,7 +15,7 @@ setup(
     author = "Meng Ke",
     author_email = "m1engk1e@gmail.com",
 
-    packages = [],
+    packages = find_packages(),
     include_package_data = True,
     platforms = "any",
     install_requires = [ "requests" ]
